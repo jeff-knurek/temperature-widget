106d6bdf811061515e5e22334662930ed49e544e
jeff-knurek <knurek.stuff@gmail.com>
2025-07-04 12:52:59 +0200

    update build settings to get sync working

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-modules/projects.
+buildscript {
+    repositories {
+        // google()
+        mavenCentral()
+    }
+}
+
 plugins {
     id("com.android.application") version "8.7.3" apply false
     id("org.jetbrains.kotlin.android") version "1.9.25" apply false
